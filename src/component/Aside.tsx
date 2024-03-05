afa408bae13c40bb471761c939cd385df76fbef3
rolandiartmeladze <rartmeladze@gmail.com>
2024-03-05 21:53:50 +0400

    Initial commit

---
+++ src/component/Aside.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';import logo from './logo.svg';
+import '../style/Aside.css';
+
+
+
+
+function Aside() {
+
+    return (
+<>
+<div className='Aside'>
+    <h1>User info
+        </h1>
+</div>
+</>
+        );
+}
+
+export default Aside;
---
+++ src/component/UserConteiner.tsx
@@ -0,0 +1,196 @@
+import React, { useState, useEffect } from 'react';import logo from './logo.svg';
+import '../style/UserConteiner.css';
+
+import UserIcon from '../icon/user.png';
+
+
+
+function UserConteiner() {
+
+    return (
+<>
+<div className='userTable'>
+      
+<div className='userConteinet'>
+      
+      <div className='userHeaderline'>
+        <img src={UserIcon} alt='User Icon' />
+        <samp>Test User 01 <h3>Tsalka</h3></samp>
+            <div className='headerMore'>...</div>
+      </div>
+    
+      <div className='userInfoLine'>
+                <samp><h1>რაოდენობა</h1> <h3>125.50  ლ.</h3></samp>
+                <samp><h1>ფასი</h1><h3>1.40   ₾.</h3></samp>
+      </div>
+    
+      <div className='userTotal'> 
+      <samp>
+        <h2>ღირებულება: <samp>250 ₾.</samp></h2>
+      </samp>
+      </div>
+
+
+              </div>
+      
+              <div className='userConteinet'>
+      
+      <div className='userHeaderline'>
+        <img src={UserIcon} alt='User Icon' />
+        <samp>Test User 01 <h3>Tsalka</h3></samp>
+            <div className='headerMore'>...</div>
+      </div>
+    
+      <div className='userInfoLine'>
+                <samp><h1>რაოდენობა</h1> <h3>125.50  ლ.</h3></samp>
+                <samp><h1>ფასი</h1><h3>1.40   ₾.</h3></samp>
+      </div>
+    
+      <div className='userTotal'> 
+      <samp>
+        <h2>ღირებულება: <samp>250 ₾.</samp></h2>
+      </samp>
+      </div>
+
+
+              </div>
+      
+              <div className='userConteinet'>
+      
+      <div className='userHeaderline'>
+        <img src={UserIcon} alt='User Icon' />
+        <samp>Test User 01 <h3>Tsalka</h3></samp>
+            <div className='headerMore'>...</div>
+      </div>
+    
+      <div className='userInfoLine'>
+                <samp><h1>რაოდენობა</h1> <h3>125.50  ლ.</h3></samp>
+                <samp><h1>ფასი</h1><h3>1.40   ₾.</h3></samp>
+      </div>
+    
+      <div className='userTotal'> 
+      <samp>
+        <h2>ღირებულება: <samp>250 ₾.</samp></h2>
+      </samp>
+      </div>
+
+
+              </div>
+      
+              <div className='userConteinet'>
+      
+      <div className='userHeaderline'>
+        <img src={UserIcon} alt='User Icon' />
+        <samp>Test User 01 <h3>Tsalka</h3></samp>
+            <div className='headerMore'>...</div>
+      </div>
+    
+      <div className='userInfoLine'>
+                <samp><h1>რაოდენობა</h1> <h3>125.50  ლ.</h3></samp>
+                <samp><h1>ფასი</h1><h3>1.40   ₾.</h3></samp>
+      </div>
+    
+      <div className='userTotal'> 
+      <samp>
+        <h2>ღირებულება: <samp>250 ₾.</samp></h2>
+      </samp>
+      </div>
+
+
+              </div>
+      
+              <div className='userConteinet'>
+      
+      <div className='userHeaderline'>
+        <img src={UserIcon} alt='User Icon' />
+        <samp>Test User 01 <h3>Tsalka</h3></samp>
+            <div className='headerMore'>...</div>
+      </div>
+    
+      <div className='userInfoLine'>
+                <samp><h1>რაოდენობა</h1> <h3>125.50  ლ.</h3></samp>
+                <samp><h1>ფასი</h1><h3>1.40   ₾.</h3></samp>
+      </div>
+    
+      <div className='userTotal'> 
+      <samp>
+        <h2>ღირებულება: <samp>250 ₾.</samp></h2>
+      </samp>
+      </div>
+
+
+              </div>
+              <div className='userConteinet'>
+      
+      <div className='userHeaderline'>
+        <img src={UserIcon} alt='User Icon' />
+        <samp>Test User 01 <h3>Tsalka</h3></samp>
+            <div className='headerMore'>...</div>
+      </div>
+    
+      <div className='userInfoLine'>
+                <samp><h1>რაოდენობა</h1> <h3>125.50  ლ.</h3></samp>
+                <samp><h1>ფასი</h1><h3>1.40   ₾.</h3></samp>
+      </div>
+    
+      <div className='userTotal'> 
+      <samp>
+        <h2>ღირებულება: <samp>250 ₾.</samp></h2>
+      </samp>
+      </div>
+
+
+              </div>
+              <div className='userConteinet'>
+      
+      <div className='userHeaderline'>
+        <img src={UserIcon} alt='User Icon' />
+        <samp>Test User 01 <h3>Tsalka</h3></samp>
+            <div className='headerMore'>...</div>
+      </div>
+    
+      <div className='userInfoLine'>
+                <samp><h1>რაოდენობა</h1> <h3>125.50  ლ.</h3></samp>
+                <samp><h1>ფასი</h1><h3>1.40   ₾.</h3></samp>
+      </div>
+    
+      <div className='userTotal'> 
+      <samp>
+        <h2>ღირებულება: <samp>250 ₾.</samp></h2>
+      </samp>
+      </div>
+
+
+              </div>
+              <div className='userConteinet'>
+      
+      <div className='userHeaderline'>
+        <img src={UserIcon} alt='User Icon' />
+        <samp>Test User 01 <h3>Tsalka</h3></samp>
+            <div className='headerMore'>...</div>
+      </div>
+    
+      <div className='userInfoLine'>
+                <samp><h1>რაოდენობა</h1> <h3>125.50  ლ.</h3></samp>
+                <samp><h1>ფასი</h1><h3>1.40   ₾.</h3></samp>
+      </div>
+    
+      <div className='userTotal'> 
+      <samp>
+        <h2>ღირებულება: <samp>250 ₾.</samp></h2>
+      </samp>
+      </div>
+
+
+              </div>
+
+      
+      
+      
+      </div>
+
+</>
+        );
+}
+
+export default UserConteiner;
